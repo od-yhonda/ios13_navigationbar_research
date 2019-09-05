c29187e18737d027f8b15a62aea505db70ccaa06
yhonda <yhonda@opendoor.co.jp>
2019-09-05 18:19:10 +0900

    Initial Commit

---
+++ ios13_navigationbar_researchTests/ios13_navigationbar_researchTests.swift
@@ -0,0 +1,34 @@
+//
+//  ios13_navigationbar_researchTests.swift
+//  ios13_navigationbar_researchTests
+//
+//  Created by yhonda on 2019/09/05.
+//  Copyright © 2019 yhonda. All rights reserved.
+//
+
+import XCTest
+@testable import ios13_navigationbar_research
+
+class ios13_navigationbar_researchTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
